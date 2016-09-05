4bce58f4da12c0fd991a902960f76af3c5f537fb
Stan Lea <stanlea10@gmail.com>
2016-09-04 21:06:15 -0400

    category size

--- src/main/scala/logicalguess/domain/Event.scala
+++ src/main/scala/logicalguess/domain/Event.scala
@@ -12,12 +12,12 @@ case class Event(guid: String, category: Category.Value, action: String, timesta
 
 /**Random event generation routines */
 object RandomEvent {
+  val categoryCount: Int = Category.values.size
   def apply() = Event(randomGiud, randomCategory, randomAction, timestamp)
   def randomGiud = java.util.UUID.randomUUID.toString
   def timestamp = new Date().toString()
-  def randomCategory = Category(random.nextInt(categories.size))
+  def randomCategory = Category(random.nextInt(categoryCount))
   def randomAction = actions(random.nextInt(actions.size))
   val random = new Random
-  val categories = Seq("info", "warning", "error")
   val actions = Seq("POST /event", "GET /events", "GET /")
 }
